45ed29e7e87aaf8061c423f6e7656def7a12b1c2
jno3 <joaoribeiro3891@gmail.com>
2023-02-22 22:56:17 -0300

    2nd commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,17 +23,17 @@ const Navbar = () => {
 
                 <ul className='nav-icons'>
                     <li>
-                        <a href='https://www.twitter.com' target='_blank' rel='noopener' className='nav-icon'
+                        <a href='https://www.twitter.com' target='_blank' rel="noreferrer" className='nav-icon'
                         ><i className='fab fa-facebook'></i
                         ></a>
                     </li>
                     <li>
-                        <a href='https://www.twitter.com' target='_blank' rel='noopener' className='nav-icon'
+                        <a href='https://www.twitter.com' target='_blank' rel="noreferrer" className='nav-icon'
                         ><i className='fab fa-twitter'></i
                         ></a>
                     </li>
                     <li>
-                        <a href='https://www.twitter.com' target='_blank' rel='noopener' className='nav-icon'
+                        <a href='https://www.twitter.com' target='_blank' rel="noreferrer" className='nav-icon'
                         ><i className='fab fa-squarespace'></i
                         ></a>
                     </li>
